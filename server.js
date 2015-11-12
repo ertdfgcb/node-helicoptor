d39f935de9ca3fe7a60c6cb0df44222c1ddb481c
Paul O'Day <paul.oday@active911.com>
2015-11-12 13:04:17 -0800

    added reciever and server

---
+++ server.js
@@ -0,0 +1,39 @@
+var net = require('net');
+var joystick = new (require('./node-joystick/joystick'))(0, 3500, 350);
+
+
+var HOST = '192.168.1.80'
+var PORT = 5000;
+
+console.log('starting');
+
+var client = new net.Socket();
+
+function send (data) {
+    var str = JSON.stringify(data);
+    console.log(str);
+    client.write(str);
+}
+
+client.connect(PORT, HOST, function() {
+
+    joystick.on('button', send);
+    joystick.on('axis', send);
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+//    client.write("Joystick server connected");
+
+});
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+    
+    console.log('DATA: ' + data);
+    // Close the client socket completely
+    
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+});
