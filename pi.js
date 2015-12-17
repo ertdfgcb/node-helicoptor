07928037debec651fe441eb9c10ab9a9e531d255
ertdfgcb <odaypaul@gmail.com>
2015-12-16 18:40:45 -0800

    clean up pi code

--- pi.js
+++ pi.js
@@ -1,36 +1,29 @@
 var net = require('net'),
 	config = require('./config.js');
 
-var joystick = new (require('joystick'))(0, config.deadzone, config.sensitivity); 
+console.log('Starting joystick server');
 
-var HOST = 'localhost'
-var PORT = 5000;
-
-console.log('starting');
-
-var client = new net.Socket();
+var joystick = new (require('joystick'))(0, config.deadzone, config.sensitivity),
+	host = 'localhost',
+	port = 5000,
+	client = new net.Socket();
 
 function send (data) {
     var str = JSON.stringify(data);
     client.write(str);
 }
 
-client.connect(PORT, HOST, function() {
+client.connect(port, host`, function() {
     joystick.on('button', send);
     joystick.on('axis', send);
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-//    client.write("Joystick server connected");
 
 });
 
-// Add a 'data' event handler for the client socket
-// data is what the server sent to this socket
 client.on('data', function(data) {
     console.log('HELICOPTOR: ' + data);
-    // Close the client socket completely
 });
 
-// Add a 'close' event handler for the client socket
 client.on('close', function() {
     console.log('Connection closed');
 });
